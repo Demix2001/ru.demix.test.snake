14edeee7fff2adf974e982394a4044f15ee78a12
Demix <Demon2001@inbox.ru>
2015-12-24 00:40:51 +0300

    изменил код

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+
+/*
+ *    Основной класс, где происходит запуск игры.
+ */
+
+
+import java.awt.*;
+
+public class Main {
+    private static final String windowName = "СУПЕР ЗМЕЙКА 3000!";    // Константа, в которой задаем название игрового окна.
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Scene scene = new Scene(windowName);        // Создаём объект класса Scene
+
+
+
+
+        new horLine ( "+", 5, 790, 10);              // Рисуем горизонтальную линию
+        new horLine ( "+", 5, 790, 570);           // Рисуем горизонтальную линию
+        new verLine ("+", 20, 570, 5);               // Рисуем вертикальную линию
+        new verLine ("+", 20, 570, 785);           // Рисуем вертикальную линию
+
+        Snake snake = new Snake(350, 300, Direction.RIGHT);
+
+        snake.draw();
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/Scene.java
@@ -0,0 +1,50 @@
+
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+/*
+ *  Класс, который создает окно и выводит на него информацию.
+ */
+
+public class Scene  {
+    private static JPanel panel1;
+    private static String text;
+    private  static int x, y;
+
+    // Конструктор класса, который создает окно.
+    public Scene(String name){
+        //super(name);           // вызываем метод супер класса (JFrame)
+        JFrame frame = new JFrame(name);
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );    // активируем остановку программы после закрытия окна.
+        frame.setSize(800, 600);          // задаем размер окна.
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);  //размещаем окно по центру экрана.
+        JPanel panel = new JPanel(new BorderLayout());
+        frame.getContentPane().add(panel);
+        panel1=panel;
+        frame.setVisible(true);          // выводим окно на экран.
+
+    }
+
+    public static void draw(String str, int _x, int _y) {
+        text = str;
+        x= _x;
+        y= _y;
+        paint(panel1.getGraphics());
+    }
+
+
+
+
+
+
+    // обязательный метод paint, который выводит информацию на фрейм.
+        public static void paint(Graphics g) {
+        // super.paint(g);
+            g.drawString(text, x, y);
+    }
+
+}
